62283b15421bbd4492618bcdee9d24fd62b25ca8
Abdul Latif Syafi'i <a.l.syafii@gmail.com>
2017-09-19 20:26:20 +0700

    Store contact api

---
+++ app/Http/Controllers/ContactAPIController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contact;
+use App\Http\Requests\StoreContactRequest;
+use Illuminate\Http\Request;
+
+class ContactAPIController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreContactRequest $request)
+    {
+        $contact = Contact::create($request->only('name', 'email', 'phone'));
+        return $contact;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/', function(){
 	return ['success' => true, 'message' => 'api index'];
 });
+
+Route::resource('contact', 'ContactAPIController');
